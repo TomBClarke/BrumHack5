c485511f03cba13f4649f35294459ac4d4013577
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-10-30 00:03:30 +0100

    some basic handling of message in PHP

--- Web/index.php
+++ Web/index.php
@@ -5,7 +5,22 @@
 
     $sender = $_REQUEST['From'];
     $message = $_REQUEST['Body'];
-    $response = "SENDER=" . $sender . "|MESSAGE=" . $message;
+    
+    if ($sender == "" || $message == "") {
+        $response = "Please play our game - Brainy Bird";
+    } else {
+        $response = "SENDER=" . $sender . "|";
+        if (strpos($message, '<Score>') !== false) {
+            try {
+                $scoreXml = new SimpleXMLElement($message);
+                $response = $response . "Score:" . $scoreXml->Score;
+            } catch (Exception $e) {
+                $response = $response . "Score:error";
+            }
+        } else {
+            $response = $response . "Query:Should check for '" . $message . "' for their score.";
+        }
+    }
 ?>
 <Response>
     <Message>
