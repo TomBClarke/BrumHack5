a084ec2a01b0769eafe30de563544892b58f0962
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-10-29 23:48:19 +0100

    trying to send back sender and response

--- Web/index.php
+++ Web/index.php
@@ -1,9 +1,14 @@
 <?php 
     header("content-type: text/xml"); 
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"; 
+    $response = "";
+
+    $sender = $_REQUEST['From'];
+    $message = $_REQUEST['Body'];
+    $response = "SENDER=" . $sender . "|MESSAGE=" . $message;
 ?>
 <Response>
     <Message>
-        TEST1
+        <?php echo $response; ?>
     </Message>
 </Response>
