67a9b2a76d9a1315394d8f877f7e7fedd87782ed
Tom Clarke <tomclarke96@hotmail.co.uk>
2016-10-29 23:45:01 +0100

    trying to test if it works :D

--- Web/index.php
+++ Web/index.php
@@ -1,47 +1,9 @@
-<?php
-
-$fp = fopen("data.txt", 'w');
-fwrite($fp, "Last request: " . date("Y-m-d h:i:sa"));
-
-if (isset($_REQUEST['From']) && isset($_REQUEST['Body'])) {
-    header("content-type: text/xml");
-    $sender = $_REQUEST['From'];
-    $message = $_REQUEST['Body'];
-    $response = "SENDER=" . $sender . "|MESSAGE=" . $message;
-} elseif(isset($_REQUEST['From'])) {
-    header("content-type: text/xml");
-    $response = "Please play our game - Brainy Bird!";
+<?php 
+    header("content-type: text/xml"); 
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"; 
 ?>
-
-<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Message><?php echo $response; ?></Message>
+    <Message>
+        TEST1
+    </Message>
 </Response>
-
-<?php } else { ?>
-
-<html>
-<head>
-    <title>Brainy Bird</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <style>
-        body {
-            text-align: center;
-            font-family: sans-serif;
-        }
-        
-        h1 {
-            position: relative;
-            top: 40%;
-        }
-    </style>
-</head>
-<body>
-    <h1>Please play our game - Brainy Bird!</h1>
-</body>
-</html>
-
-<?php } ?>
-
-
---
+++ Web/tobebetter.php
@@ -0,0 +1,42 @@
+<?php
+
+if (isset($_REQUEST['From']) && isset($_REQUEST['Body'])) {
+    header("content-type: text/xml");
+    $sender = $_REQUEST['From'];
+    $message = $_REQUEST['Body'];
+    $response = "SENDER=" . $sender . "|MESSAGE=" . $message;
+} elseif(isset($_REQUEST['From'])) {
+    header("content-type: text/xml");
+    $response = "Please play our game - Brainy Bird!";
+?>
+
+<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Message><?php echo $response; ?></Message>
+</Response>
+
+<?php } else { ?>
+
+<html>
+<head>
+    <title>Brainy Bird</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style>
+        body {
+            text-align: center;
+            font-family: sans-serif;
+        }
+        
+        h1 {
+            position: relative;
+            top: 40%;
+        }
+    </style>
+</head>
+<body>
+    <h1>Please play our game - Brainy Bird!</h1>
+</body>
+</html>
+
+<?php } ?>
